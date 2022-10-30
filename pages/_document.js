eb7b83d64aaf20c6097a333f24dc30a4bbf6a0bc
Slade Harker <slade_harker@hotmail.com>
2022-10-31 08:04:50 +1000

    add fonts to project

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,16 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import Link from "next/link";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+            <link rel="preload" hefre="/fonts/IBMPlexSans-Bold.tff" as="font" crossOrigin="anonymous"></link>
+            <link rel="preload" hefre="/fonts/IBMPlexSans-Italic.tff" as="font" crossOrigin="anonymous"></link>
+            <link rel="preload" hefre="/fonts/IBMPlexSans-Regular.tff" as="font" crossOrigin="anonymous"></link>
+            <link rel="preload" hefre="/fonts/IBMPlexSans-SemiBold.tff" as="font" crossOrigin="anonymous"></link>
+        </Head>
         <body>
           <Main></Main>
           <NextScript />
